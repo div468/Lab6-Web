93384f1bd2d6643c4df88e588571de9f67f69654
div468 <julianandre.divas@gmail.com>
2026-03-17 14:24:58 -0600

    Added scroll controll

--- static/script.js
+++ static/script.js
@@ -1,37 +1,59 @@
+let renderedMessages = 0
+let message_sent = false
+
 const getMessages = async () => {
+    const ul = document.getElementById("messages")
+
+    const nearBottom = ul.scrollTop + ul.clientHeight >= ul.scrollHeight - 10
+
     const response = await fetch("/api/messages")
     const messages = await response.json()
-    const ul = document.getElementById("messages")
-    ul.innerHTML = ""
 
-    for (let i = 0; i < messages.length; i++){
-        const message = messages[i]
-        const li = document.createElement("li")
-        li.innerHTML = `<strong>${message.user}:</strong> ${message.text}`
-        ul.append(li)
+    if (messages.length > renderedMessages) {
+
+        for (let i = renderedMessages; i < messages.length; i++) {
+            const message = messages[i]
+
+            const li = document.createElement("li")
+            li.innerHTML = `<strong>${message.user}:</strong> ${message.text}`
+
+            ul.append(li)
+        }
+
+        if (nearBottom || message_sent) {
+            ul.scrollTop = ul.scrollHeight
+        }
+
+        renderedMessages = messages.length
+        message_sent = false
     }
 }
 
 const postMessage = async (message) => {
+    message_sent = true
+
     await fetch("/api/messages", {
-        method:"POST",
-         body:JSON.stringify(message) 
-        })
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(message)
+    })
 
     getMessages()
 }
 
-setInterval(() =>{
+setInterval(() => {
     getMessages()
 }, 1000)
 
-document.getElementById("send").addEventListener("click", ()=>{
+document.getElementById("send").addEventListener("click", () => {
     const textArea = document.getElementById("message")
-    const message = textArea.value
-    postMessage(
-        {
-            user:"Julián",
-            text:message,
-        }
-    )
+
+    postMessage({
+        user: "Julián",
+        text: textArea.value
+    })
+
+    textArea.value = ""
 })
